2ebb083aab3bb9afe222a2f71277cf945b8ebc23
Chetan Krishna Kodeboyina <chetan.kodeboyina@kcl.ac.uk>
2022-10-09 19:22:41 +0100

    Add contraint in model

--- things/models.py
+++ things/models.py
@@ -4,8 +4,8 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Thing(Model):
-    name = models.CharField(max_length=30, black=False, unique=True)
-    description = models.CharField(max_length=120, black=True)
+    name = models.CharField(max_length=30, blank=False, unique=True)
+    description = models.CharField(max_length=120, blank=True)
     quantity = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(100)]
     )
