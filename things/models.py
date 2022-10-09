dc3014035d11e94c76cb2c3dd902f74077909491
Chetan Krishna Kodeboyina <chetan.kodeboyina@kcl.ac.uk>
2022-10-09 19:18:40 +0100

    Add contraint to variables in model

--- things/models.py
+++ things/models.py
@@ -1,9 +1,11 @@
-from unicodedata import name
 from django.db import models
 from django.db.models import Model
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Thing(Model):
-    name = models.TextField()
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, black=False, unique=True)
+    description = models.CharField(max_length=120, black=True)
+    quantity = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(100)]
+    )
