a1192d0d18ce6edfa65d094183735e20bd14d0cf
Chetan Krishna Kodeboyina <chetan.kodeboyina@kcl.ac.uk>
2022-10-09 19:03:52 +0100

    Create Thing model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,9 @@
+from unicodedata import name
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+
+class Thing(Model):
+    name = models.TextField()
+    description = models.TextField()
+    quantity = models.IntegerField()
