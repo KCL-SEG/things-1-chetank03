9257624d4440306bd27ba33c8a96811a16ad3132
Chetan Krishna Kodeboyina <chetan.kodeboyina@kcl.ac.uk>
2022-10-15 13:14:59 +0100

    make migration after editing the models.py

--- things/models.py
+++ things/models.py
@@ -7,5 +7,5 @@ class Thing(Model):
     name = models.CharField(max_length=30, blank=False, unique=True)
     description = models.CharField(max_length=120, blank=True)
     quantity = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(100)]
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
     )
